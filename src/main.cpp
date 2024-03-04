3b9eb34b65085387ee75acbde04335c0919768b4
Lucas Andreatta <drea.pierra@gmail.com>
2024-03-03 21:09:03 -0300

    changed how the different arguments are handled, for 'decode' and 'info'

--- src/main.cpp
+++ src/main.cpp
@@ -8,57 +8,70 @@
 
 int main(int argc, char *argv[])
 {
-        // If there are less than 3 arguments or the first argument is not "decode", print usage and exit
-        if (argc < 3 || std::string_view(argv[1]) != "decode")
+        if (argc < 3)
         {
-                std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
+                std::cerr << "Usage: " << argv[0] << " <command> <encoded_value>" << std::endl;
                 return 1;
         }
 
+        std::string_view command(argv[1]);
         std::string_view encoded_value(argv[2]);
 
-        json decoded_value;
-        try
+        if (command == "decode")
         {
-                switch (encoded_value.front())
+                json decoded_value;
+                try
                 {
-                case 'i':
-                {
-                        auto [value, _] = decode_bencoded_integer(encoded_value);
-                        decoded_value = value;
-                        break;
-                }
-                case 'l':
-                {
-                        auto [value, _] = decode_bencoded_list(encoded_value);
-                        decoded_value = value;
-                        break;
-                }
-                case 'd':
-                {
-                        auto [value, _] = decode_bencoded_dictionary(encoded_value);
-                        decoded_value = value;
-                        break;
-                }
-                default:
-                        if (std::isdigit(encoded_value.front()))
+                        switch (encoded_value.front())
                         {
-                                auto [value, _] = decode_bencoded_string(encoded_value);
+                        case 'i':
+                        {
+                                auto [value, _] = decode_bencoded_integer(encoded_value);
                                 decoded_value = value;
+                                break;
                         }
-                        else
+                        case 'l':
                         {
-                                throw std::invalid_argument("Invalid bencode type");
+                                auto [value, _] = decode_bencoded_list(encoded_value);
+                                decoded_value = value;
+                                break;
+                        }
+                        case 'd':
+                        {
+                                auto [value, _] = decode_bencoded_dictionary(encoded_value);
+                                decoded_value = value;
+                                break;
+                        }
+                        default:
+                                if (std::isdigit(encoded_value.front()))
+                                {
+                                        auto [value, _] = decode_bencoded_string(encoded_value);
+                                        decoded_value = value;
+                                }
+                                else
+                                {
+                                        throw std::invalid_argument("Invalid bencode type");
+                                }
                         }
                 }
+                catch (const std::invalid_argument &e)
+                {
+                        std::cerr << "Error decoding bencoded value: " << e.what() << std::endl;
+                        return 1;
+                }
+
+                std::cout << decoded_value.dump() << std::endl;
         }
-        catch (const std::invalid_argument &e)
+        else if (command == "info")
         {
-                std::cerr << "Error decoding bencoded value: " << e.what() << std::endl;
+                // Handle info command
+        }
+        else
+        {
+                std::cerr << "Unknown command: " << command << std::endl;
+                std::cerr << "Usage: " << argv[0] << " <command> <argument>" << std::endl;
                 return 1;
         }
-
-        std::cout << decoded_value.dump() << std::endl;
 
         return 0;
 }
